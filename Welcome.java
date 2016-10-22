a001ad515c46ddb6fdec98d75622f458156ed6e5
unknown <>
2016-10-22 12:09:59 +0800

    msg

---
+++ Welcome.java
@@ -0,0 +1,11 @@
+package hello;
+
+public class Welcome 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("******************************");
+		System.out.println("*   Welcome to java World!   *");
+		System.out.println("******************************");
+	}
+}
